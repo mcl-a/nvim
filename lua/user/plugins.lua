668cde4e29c002410f6ffa8b94198e8bcf1441b1
amcl <amcl@duck.com>
2023-09-20 21:49:48 -0400

    Updates

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -50,7 +50,10 @@ return {
       'nvim-lua/plenary.nvim',
       {
         'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+        config = function()
+          require('telescope').load_extension('fzf')
+        end
       },
       {
         'ahmedkhalf/project.nvim',
--- lua/user/setup.lua
+++ lua/user/setup.lua
@@ -147,14 +147,14 @@ require('telescope').setup {           -- defaults
       fuzzy = true,                    -- false will only do exact matching
       override_generic_sorter = true,  -- override the generic sorter
       override_file_sorter = true,     -- override the file sorter
-      case_mode = 'smart_case',        -- or 'ignore_case' or 'respect_case'
-                                       -- the default case_mode is 'smart_case'
+      case_mode = 'smart_case',        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
     }
   }
 }
 
 -- Enable telescope fzf native
-pcall(require('telescope').load_extension('fzf'))
+-- pcall(require('telescope').load_extension('fzf'))
 
 -- WHICH-KEY
 require('which-key').setup{
